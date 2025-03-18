7272a1ad942e04bf4e47444bf9743a2bb5e20267
Julian <julianjometzger@gmail.com>
2025-03-18 13:20:37 +0100

    feat: Now main is checking for file parity.

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#include "lib/file-check.h"
+
 int main(int argc, char **argv) {
-  printf("Testowanie programu 'jimp2-graphs'.");
+  printf("Testowanie programu 'jimp2-graphs'.\n");
+  int file_parity = checkFileParity("test.txt", 3, 3);
+  printf("File parity sum: %d", file_parity);
 }
