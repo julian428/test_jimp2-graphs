91213c6504f31cd7bfee2f17c5af455110392f70
Julian <julianjometzger@gmail.com>
2025-03-20 09:03:16 +0100

    fix: Added the getNodes function to the header file.

--- lib/file-check.h
+++ lib/file-check.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *getNodes(FILE *, const int);
 int checkFileParity(char *, const int, const int);
 int checkGraphParity(char *, const int, const int);
 
